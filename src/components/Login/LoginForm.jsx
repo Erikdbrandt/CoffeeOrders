07e3c3bc966268e0f6b5863d1fd6e9a05dc8df90
Erik Brandt <erik.brandt@se.experis.com>
2023-01-19 21:05:08 +0100

    Username Form usernameConfig now working

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,33 +1,54 @@
-import { useForm } from 'react-hook-form'
+import {useForm} from 'react-hook-form'
 
 
 const usernameConfig = {
     required: true,
-    minLength: 2
+    minLength: 3
 }
 const LoginForm = () => {
 
-        const {
-            register,
-            handleSubmit,
-            formState: {errors}
-        } = useForm()
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm()
 
-        const onSubmit = (data) => {
-            console.log(data)
+    const onSubmit = (data) => {
+        console.log(data)
+
+    }
+
+    console.log(errors)
+
+
+    const errorMessage = (() => {
+        if(!errors.username) {
+            return null
         }
 
+        if(errors.username.type === 'required'){
+           return <span>Username is required</span>
+        }
+
+        if (errors.username.type === 'minLength'){
+            return <span>Username is too short (min 3 characters)</span>
+        }
+
+    })()
 
     return (
         <>
             <h2>What's your name?</h2>
-            <form onSubmit={ handleSubmit(onSubmit) }>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <fieldset>
                     <label htmlFor="username">Username: </label>
                     <input
                         type="text"
                         placeholder="johnDoe"
                         {...register("username", usernameConfig)} />
+                    {errorMessage }
+
+
                 </fieldset>
 
                 <button type="submit">Continue</button>
