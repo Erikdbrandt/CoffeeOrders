842deffe7a779de01ff2d76da0c239c1f972fd05
Erik Brandt <erik.brandt@se.experis.com>
2023-01-20 14:17:07 +0100

    start av 10

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,9 +1,22 @@
 import withAuth from "../hoc/withAuth"
+import ProfileHeader from "../components/Profile/ProfileHeader"
+import ProfileActions from "../components/Profile/ProfileActions"
+import ProfileOrderHistory from "../components/Profile/ProfileOrderHistory"
+import {useUser} from "../context/UserContext"
 const Profile = () => {
 
+
+    const {user} = useUser()
+
     return (
+        <>
         <h1>Profile</h1>
-    )
+            <ProfileHeader username={user.username} />
+            <ProfileActions/>
+            <ProfileOrderHistory orders={user.orders}/>
+        </>
+
+)
 }
 
 export default withAuth(Profile)
