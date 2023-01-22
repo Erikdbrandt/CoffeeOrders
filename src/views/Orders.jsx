3035d2165897d415d0f79cecc5dd37d3688f46e5
Erik Brandt <erik.brandt@se.experis.com>
2023-01-22 20:18:24 +0100

    Klar med 12

--- src/views/Orders.jsx
+++ src/views/Orders.jsx
@@ -1,25 +1,59 @@
 import withAuth from "../hoc/withAuth"
 import OrdersCoffeeButton from "../components/Orders/OrdersCoffeeButton"
 import OrdersForm from "../components/Orders/OrdersForm"
+import {useState} from "react"
 
 
-
+const COFFEES = [
+    {
+        id: 1,
+        name: "Americano",
+        image: "img/americano.png"
+    },
+    {
+        id: 2,
+        name: "Cappucciono",
+        image: "img/cappuccino.png"
+    },
+    {
+        id: 3,
+        name: "Latte",
+        image: "img/latte.png"
+    },
+    {
+        id: 4,
+        name: "Espresso",
+        image: "img/espresso.png"
+    },
+]
 
 const Orders = () => {
+
+    const [coffee,setCoffee] = useState(null)
+    const handleCoffeeClicked = (coffeeId) => {
+        setCoffee(COFFEES.find(coffee => coffee.id === coffeeId))
+    }
+
+    const availableCoffees = COFFEES.map(coffee => {
+        return <OrdersCoffeeButton
+            key={coffee.id}
+            coffee={coffee}
+            onSelect={handleCoffeeClicked}
+        />
+    })
 
     return (
         <>
             <h1>Orders</h1>
             <section id="coffee-options">
-                <OrdersCoffeeButton name="Americano" image="img/americano.png" key="americano"> </OrdersCoffeeButton>
-                <OrdersCoffeeButton name="Cappuccino" image="img/cappuccino.png" key="cappuccino"></OrdersCoffeeButton>
-                <OrdersCoffeeButton name="Latte" image="img/latte.png" key="latte"></OrdersCoffeeButton>
-                <OrdersCoffeeButton name="Espresso" image="img/espresso.png" key="espresso"></OrdersCoffeeButton>
+                {availableCoffees}
 
             </section>
             <section id="order-notes">
                 <OrdersForm/>
             </section>
+            <h4>Summary: </h4>
+            {coffee && <p>Selected coffee: { coffee.name}</p> }
         </>
 
     )
